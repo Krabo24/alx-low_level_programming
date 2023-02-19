53a78eb6b94ad362fd017be509497375624a9bad
Krabs <krabo24@gmail.com>
2023-02-19 12:00:30 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print the alphabet in lower case
+ * and uppercase on a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
