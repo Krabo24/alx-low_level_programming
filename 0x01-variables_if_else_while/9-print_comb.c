e32fe6ed02e7bc805b78704f749a5ed58318d0cf
Krabs <krabo24@gmail.com>
2023-02-20 13:32:47 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main combinations
+ * Description: rogram that prints all possible combinations of single-digit numbers.
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
