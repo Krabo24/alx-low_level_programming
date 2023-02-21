b01fc3874b3077e39678120b63e6ab8e41644c5c
Krabs <krabo24@gmail.com>
2023-02-21 13:34:42 -0800

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - main
+ * main - main block
  * Descrition: program that finds and prints the sum of the even-valued terms,
  * followed by a new line.
  * Return: 0
@@ -18,6 +18,6 @@ int main(void)
 		if (next % 2 == 0)
 			sum += next;
 	}
-	printf("%\n", sum);
+	printf("%i\n", sum);
 	return (0);
 }
