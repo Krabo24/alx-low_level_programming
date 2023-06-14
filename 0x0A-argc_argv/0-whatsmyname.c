141b89ac4ff031b24d6fecb85b24510e75b51a98
Krabs <krabo24@yahoo.com>
2023-06-14 13:08:21 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-
 #include "main.h"
 
-
-
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - Will print the name of the program
+ * @argc: is where the number of arguments will be recorded
+ * @argv: Is the array of arguments in our program
+ * Return: 0 Success
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
-
 {
-
 	printf("%s\n", *argv);
 
-
-
 	return (0);
-
 }
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,17 @@
-0x0A. C - argc, argv
+C - argc, argv
+		
+
+		
+TASKS		
+		
+Task 0 we will Write a program that prints its name, followed by a new line.		
+
+Task 1 Silence is argument carried out by other means](1-args.c)
+
+We will be writting a program that prints all arguments it receives. for task 2		
+
+Task 3. Neither irony nor sarcasm is argument](3-mul.c)
+		
+In task 4 we will  write a program that adds positive numbers to it
+		
+Task 5 Minimal Number of Coins for Change](100-change.c)
